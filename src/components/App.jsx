d50fea04bf8592e1bd313669ae68c7078079cc5a
ArtemKotlar <artemkotlar93@gmail.com>
2023-03-25 01:14:17 +0200

    фон

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,7 +5,7 @@ import { Box } from './Box';
 
 const App = () => {
   return (
-    <Box bg="box" pt={2} pb={8}>
+    <Box backgroundImage="url(https://kartinkin.net/uploads/posts/2021-07/1625166379_38-kartinkin-com-p-fon-gori-krasivie-foni-42.jpg)" pt={2} pb={8}>
       <h1 style={{ textAlign: 'center' }}>Phonebook</h1>
       <Forms />
       <h2 style={{ textAlign: 'center' }}>Contacts</h2>
@@ -16,3 +16,7 @@ const App = () => {
 };
 
 export default App;
+
+
+
+// https://mobimg.b-cdn.net/v3/fetch/af/af4e684497699463ff4813eaaabf2e90.jpeg
--- src/components/Box.jsx
+++ src/components/Box.jsx
@@ -1,4 +1,24 @@
 import styled from 'styled-components';
-import { color, space, layout, flexbox, border, shadow } from 'styled-system';
+import {
+  color,
+  space,
+  layout,
+  flexbox,
+  grid,
+  background,
+  border,
+  position,
+  shadow,
+} from 'styled-system';
 
-export const Box = styled('div')(color, space, layout, flexbox, border, shadow);
+export const Box = styled('div')(
+  color,
+  space,
+  layout,
+  flexbox,
+  grid,
+  background,
+  border,
+  position,
+  shadow
+);
